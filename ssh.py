0ac4355206e6084904f60a68cb36d088159e15af
Azlux <github@azlux.fr>
2018-10-22 16:02:57 +0200

    better search

--- ssh.py
+++ ssh.py
@@ -8,6 +8,9 @@ import getch
 import argparse
 import re
 
+additional_config = ""
+search = ""
+
 
 def clear():
     _ = system_call('cls' if system_name == 'nt' else 'clear')
@@ -42,23 +45,34 @@ def ctrl_caught(signal, frame):
 
 
 def change_name(name):
-    _ = system_call('if [ -n "$TMUX" ]; then tmux rename-window -t${TMUX_PANE} \"" + name + "\";fi')
+    _ = system_call('if [ -n "$TMUX" ]; then tmux rename-window -t${TMUX_PANE} "' + name + '";fi')
 
 
-def create_table_for_prompt(table, position, search):
+def start_ssh(parameter):
+    change_name(parameter)
+    if additional_config:
+        execlp('/usr/bin/env', '/usr/bin/env', 'bash', '-c',
+               'ssh -oStrictHostKeyChecking=no -F ' + additional_config + ' ' + parameter + ';if [ -n "$TMUX" ]; then tmux rename-window -t${TMUX_PANE} $(hostname);fi')
+    else:
+        execlp('/usr/bin/env', '/usr/bin/env', 'bash', '-c',
+               'ssh -oStrictHostKeyChecking=no ' + parameter + ';if [ -n "$TMUX" ]; then tmux rename-window -t${TMUX_PANE} $(hostname);fi')
+
+
+def create_table_for_prompt(table, position):
     new_table = []
     match = re.match(r"d/(\S+)/(.*)", search)
     folder = ""
+    search_tp = ""
     if match:
         folder = match.groups()[0]
-        search = match.groups()[1]
+        search_tp = match.groups()[1]
 
     for i in table:
-        if i[2] and not folder and not search:
+        if i[2] and not folder and not search_tp:
             if [' ', str('d/' + i[2]) + '/', ''] not in new_table:
                 new_table.append([' ', str('d/' + i[2]) + '/', ''])
 
-        elif (search and search.lower() in i[0].lower()) or (not search and not folder) or (folder and not search and folder == i[2]):
+        elif (search_tp and search_tp.lower() in i[0].lower()) or (not search_tp and not folder) or (folder and not search_tp and folder == i[2]):
             i2 = i.copy()
             i2.insert(0, ' ')
             new_table.append(i2)
@@ -97,7 +111,7 @@ def get_ssh_server(path=Path(Path.home() / ".ssh/config")):
         return all_ssh, max_length + 4
 
 
-def print_prompt(table, search="", max_len=20, ):
+def print_prompt(table, max_len=20):
     clear()
     print("\n\n\t\t\tServer List\n\n\t\t********************************\n\t\t* Type to search : {}\n\t\t********************************\n".format(search))
 
@@ -113,15 +127,14 @@ def print_prompt(table, search="", max_len=20, ):
             print("{}{}{}".format(i[1], " " * (max_len - len(i[1])), i[2]))
 
 
-def start_prompter(ssh_table, max_len=20, search="", system_argv=None):
+def start_prompter(ssh_table, max_len=20, system_argv=None):
     pos = 0
-    table = create_table_for_prompt(ssh_table, position=pos, search=search)
+    global search
+    table = create_table_for_prompt(ssh_table, position=pos)
     if len(table) == 0:
-        change_name(system_argv[0])
-        execlp('/usr/bin/env', '/usr/bin/env', 'bash', '-c',
-               'ssh -oStrictHostKeyChecking=no ' + system_argv[0] + ';if [ -n "$TMUX" ]; then tmux rename-window -t${TMUX_PANE} $(hostname);fi')
+        start_ssh(system_argv[0])
 
-    print_prompt(table, search=search, max_len=max_len)
+    print_prompt(table, max_len=max_len)
     while True:
         char = get_input()
 
@@ -142,14 +155,12 @@ def start_prompter(ssh_table, max_len=20, search="", system_argv=None):
         else:
             search += char
 
-        table = create_table_for_prompt(ssh_table, position=pos, search=search)
-        print_prompt(table, search=search, max_len=max_len)
+        table = create_table_for_prompt(ssh_table, position=pos)
+        print_prompt(table, max_len=max_len)
 
     if len(table) == 0:
         exit()
-    change_name(table[pos % len(table)][1])
-    execlp('/usr/bin/env', '/usr/bin/env', 'bash', '-c',
-           'ssh -oStrictHostKeyChecking=no ' + table[pos % len(table)][1] + ';if [ -n "$TMUX" ]; then tmux rename-window -t${TMUX_PANE} $(hostname);fi')
+    start_ssh(table[pos % len(table)][1])
 
 
 def main():
@@ -160,23 +171,23 @@ def main():
 
     ssh_server, max_len = get_ssh_server()
     if args.additional_config:
+        global additional_config
+        additional_config = args.additional_config
         tp1, tp2 = get_ssh_server(Path(args.additional_config))
         ssh_server = ssh_server + tp1
         max_len = (max(max_len, tp2))
     all_host = [i[0] for i in ssh_server]
-    search = ""
     if len(system_argv) < 2:
         if len(system_argv) == 1 and system_argv[0] in all_host:
-            change_name(system_argv[0])
-            execlp('/usr/bin/env', '/usr/bin/env', 'bash', '-c',
-                   'ssh -oStrictHostKeyChecking=no ' + system_argv[0] + ";if [ -n \"$TMUX\" ]; then tmux rename-window -t${TMUX_PANE} $(hostname);fi")
+            start_ssh(system_argv[0])
         elif len(system_argv) == 1:
+            global search
             search = system_argv[0]
     else:
         execlp('ssh', 'ssh', *system_argv)
 
     rows, _ = popen('stty size', 'r').read().split()
-    start_prompter(ssh_server, max_len, search, system_argv)
+    start_prompter(ssh_server, max_len, system_argv)
 
 
 if __name__ == '__main__':
