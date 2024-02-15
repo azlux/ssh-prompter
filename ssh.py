565e5479f0a22c8f4982bb3c07ce7f88ece65ef1
azlux <github@azlux.fr>
2024-02-15 18:26:45 +0100

    ScrollBar

--- ssh.py
+++ ssh.py
@@ -1,18 +1,17 @@
-#! /usr/bin/env python3.6
+#!/usr/bin/env python3
 
 from os import system as system_call
-from os import name as system_name
 from os import execlp
-from os import popen
 from pathlib import Path
 import signal
 import argparse
 import re
 import curses
-import socket
+import filecmp
+from typing import Optional
 
 search = ""
-args = None
+args: Optional[argparse.ArgumentParser] = None
 
 
 def ctrl_caught(signal, frame):
@@ -24,38 +23,18 @@ def ctrl_caught(signal, frame):
         pass
     exit(1)
 
-
-def is_ssh_open(host, ip, port):
-    if not ip:
-        ip = host
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)
-    try:
-        s.connect((ip, port))
-        s.shutdown(socket.SHUT_RDWR)
-        return True
-    except (socket.gaierror, socket.herror):
-        return True
-    except (socket.timeout, ConnectionRefusedError):
-        return False
-    finally:
-        s.close()
-
-
 def change_name(name):
     system_call('if [ -n "$TMUX" ]; then tmux rename-window -t${TMUX_PANE} "' + name + '";fi; if [ -n "$SECURECRT" ]; then echo -n "\033]2;"' + name + '"\007";fi')
 
 
-def start_ssh(host, ip="", port=22, ping=True):
+def start_ssh(host, ip="", port=22):
     change_name(host)
-    if not args.fallback or not ping or (args.fallback and is_ssh_open(host, ip, port)):
+    if not args.color:
         execlp('/usr/bin/env', '/usr/bin/env', 'bash', '-c',
                'ssh -oStrictHostKeyChecking=no ' + host + ';if [ -n "$TMUX" ]; then tmux rename-window -t${TMUX_PANE} $(hostname);fi; if [ -n "$SECURECRT" ]; then echo -n "\033]2;$(hostname)\007";fi')
     else:
-        if not ip:
-            ip = host
-        print("***CONNEXION EN TELNET ***\n")
-        execlp('/usr/bin/env', '/usr/bin/env', 'bash', '-c', 'telnet ' + ip + ';if [ -n "$TMUX" ]; then tmux rename-window -t${TMUX_PANE} $(hostname);fi; if [ -n "$SECURECRT" ]; then echo -n "\033]2;$(hostname)\007";fi')
+        execlp('/usr/bin/env', '/usr/bin/env', 'bash', '-c',
+               'ssh -oStrictHostKeyChecking=no ' + host + ' | ct ;if [ -n "$TMUX" ]; then tmux rename-window -t${TMUX_PANE} $(hostname);fi; if [ -n "$SECURECRT" ]; then echo -n "\033]2;$(hostname)\007";fi')
 
 
 def create_table_for_prompt(table):
@@ -80,36 +59,30 @@ def create_table_for_prompt(table):
 
 def get_ssh_server(path=Path(Path.home() / ".ssh/config")):
     all_ssh = []
-    max_length = 0
     if path.is_file():
         with open(path) as f:
             host = ""
             hostname = ""
             folder = ""
             port = 22
-            ping = True
             for line in f:
                 line = line.strip()
-                if len(line) > 0 and line[0] != "#" :
+                if len(line) > 0 and line[0] != "#":
                     if 'include' in line.lower() and len(line.split(" ")) == 2:
                         path_include = line.split(" ")[1]
                         if path_include[0] in ['/', '~']:
                             pass
                         else:
-                            path_include = "~/" + path_include
-                        new_ssh, new_max = get_ssh_server(path=Path(path_include).expanduser())
+                            path_include = "~/.ssh/" + path_include
+                        new_ssh = get_ssh_server(path=Path(path_include).expanduser())
                         all_ssh = all_ssh + new_ssh
-                        max_length = max(new_max, max_length)
                     if "host " in line.lower() and "*" not in line.lower():
                         if host:
-                            all_ssh.append([host, hostname, folder, port, ping])
-                            if len(host) > max_length:
-                                max_length = len(host)
+                            all_ssh.append([host, hostname, folder, port])
                             host = ""
                             hostname = ""
                             folder = ""
                             port = 22
-                            ping = True
                     if "host " in line.lower() and len(line.split(" ")) == 2 and "*" not in line.lower():
                         host = line.split(" ")[1]
                     elif host:
@@ -119,15 +92,13 @@ def get_ssh_server(path=Path(Path.home() / ".ssh/config")):
                             folder = line.split(" ")[1]
                         elif "port" in line.lower() and len(line.split(" ")) == 2:
                             port = int(line.split(" ")[1])
-                        elif "proxy" in line.lower() and len(line.split(" ")) > 1:
-                            ping = False
-            all_ssh.append([host, hostname, folder, port, ping])
-        return all_ssh, max_length
+            all_ssh.append([host, hostname, folder, port])
+        return all_ssh
 
 
-def start_prompter(ssh_table, max_len=20, system_argv=None):
-    position = 0
+def start_prompter(ssh_table, system_argv=None):
     global search
+    previous_search = ""
     table = create_table_for_prompt(ssh_table)
     if len(table) == 0:
         start_ssh(system_argv[0])
@@ -145,18 +116,23 @@ def start_prompter(ssh_table, max_len=20, system_argv=None):
     height, width = screen.getmaxyx()
     title_position_y = 1
 
-    box_y = 64
     box_position_y = title_position_y + 3
-    position_x = max(int((width - 64) / 3 - 1), 0)
+    box_y = max(int(width / 2), 64)
+    position_x = max(int((width / 2 - box_y / 2)), 0)
 
-    max_row = max(height - box_position_y - 3, 5)
-
+    max_lines = max(height - box_position_y - 3, 5)
+    max_len = box_y
     box_search = curses.newwin(3, box_y, title_position_y, position_x)
     box_search.box()
 
-    box = curses.newwin(max_row + 2, box_y, box_position_y, position_x)
-    box.keypad(1)
+    box = curses.newwin(max_lines + 2, box_y, box_position_y, position_x)
+    box.keypad(True)
     box.box()
+
+    cursor_position = 0
+    cursor_top = 0
+    all_entries = len(table) - 1
+    page = all_entries // max_lines
 
     while True:
         screen.erase()
@@ -166,20 +142,27 @@ def start_prompter(ssh_table, max_len=20, system_argv=None):
         box_search.border(0)
         box_search.addstr(0, 3, "Server List")
         box_search.addstr(1, 5, "Type to search : {}".format(search))
-        for p, val in enumerate(table):
-            if p > max_row:
+        # box_search.addstr(2, 5, f"debug: cursor_top:{cursor_top} cursor_position:{cursor_position} max:{max_lines} all:{all_entries} page:{page}")
+        scroll_nb = int((cursor_position * min(max_lines, all_entries)) / all_entries)
+        for nb, val in enumerate(table[cursor_top:cursor_top + max_lines]):
+            if nb >= max_lines:
                 break
-            if p == position:
+            if nb == cursor_position - cursor_top:
                 color = curses.A_REVERSE
             else:
                 color = curses.A_NORMAL
 
-            match = re.match(r"d/(\S+)/", val[0])
-
-            if match:
-                box.addstr(p + 1, 2, "+ {}".format(match.groups()[0]), color)
+            match_folder = re.match(r"d/(\S+)/", val[0])
+            if match_folder:
+                box.addstr(nb + 1, 2, f"+ {match_folder.groups()[0]}", color)
             else:
-                box.addstr(p + 1, 2, "{}{}{}".format(val[0], " " * (max_len - len(val[0])), val[1]), color)
+                if (nb - 1 <= scroll_nb <= nb + 1) or (scroll_nb < 2 and nb < 3) or (scroll_nb > max_lines - 2 and nb > max_lines - 2):
+                    scroll = '#'
+                else:
+                    scroll = '|'
+                number_space = min(80, max_len) - len(val[0]) - len(val[1]) - 5
+                box.addnstr(nb + 1, 2, f"{val[0]}{' ' * number_space}{val[1]}", box_y - 4, color)
+                box.addnstr(nb + 1, box_y - 2, f"{scroll}", box_y - 2)
 
         screen.refresh()
         box.refresh()
@@ -187,63 +170,84 @@ def start_prompter(ssh_table, max_len=20, system_argv=None):
         char = box.getch()
 
         if char == curses.KEY_UP:
-            if len(table) > 0:
-                position = (position - 1) % len(table)
-        elif char == curses.KEY_DOWN and len(table) > 0:
-            if len(table) > 0:
-                position = (position + 1) % len(table)
+            if len(table) > 0 and cursor_position > 0:
+                if cursor_position <= cursor_top + 5 and cursor_top != 0:
+                    cursor_top -= 1
+                    cursor_position -= 1
+                else:
+                    cursor_position -= 1
+        elif char == curses.KEY_DOWN:
+            if len(table) > 0 and cursor_position < all_entries:
+                if cursor_position - cursor_top >= max_lines - 5:
+                    cursor_top += 1
+                    cursor_position += 1
+                else:
+                    cursor_position += 1
+        elif char == curses.KEY_PPAGE:
+            if len(table) > 0 and cursor_position > 0:
+                cursor_top -= min(max_lines - 1, cursor_top)
+                cursor_position -= min(max_lines - 1, cursor_position)
+
+        elif char == curses.KEY_NPAGE:
+            if len(table) > 0 and cursor_position < all_entries:
+                cursor_top += min(max_lines - 1, all_entries - cursor_position)
+                cursor_position += min(max_lines - 1, all_entries - cursor_position)
+
         elif char == curses.KEY_RIGHT or char == curses.KEY_LEFT:
             pass
         elif char == curses.KEY_ENTER or char == 10:
             if len(table) == 0:
                 curses.endwin()
                 exit()
-            match = re.match(r"d/(\S+)/", table[position % len(table)][0])
+            match = re.match(r"d/(\S+)/", table[cursor_position % len(table)][0])
             if match:
-                search = table[position % len(table)][0]
+                search = table[cursor_position % len(table)][0]
             else:
                 break
         elif char == curses.KEY_DC or char == 127 or char == curses.KEY_BACKSPACE:
             search = search[:-1]
-        elif char == 27:
+        elif char == 27:  # ESC
             curses.endwin()
             exit()
         else:
             search += chr(char)
 
-        table = create_table_for_prompt(ssh_table)
-        if len(table):
-            position = position % len(table)
+        if previous_search != search:
+            table = create_table_for_prompt(ssh_table)
+            previous_search = search
+            all_entries = len(table) - 1
+            cursor_position = 0
+            cursor_top = 0
 
     curses.endwin()
 
     if len(table) == 0:
         exit()
-    start_ssh(table[position % len(table)][0], table[position % len(table)][1], port=table[position % len(table)][3], ping=table[position % len(table)][4])
+    start_ssh(table[cursor_position][0], table[cursor_position][1], port=int(table[cursor_position][3]))
 
 
 def main():
     parser = argparse.ArgumentParser(prog="Drop Menu of ~/.ssh/config file", formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("--fallback", action="store_true", dest="fallback", required=False, help="fallback into Telnet if ssh is not open")
+    parser.add_argument("--fallback", action="store_true", dest="fallback", required=False, help="USELESS")
+    parser.add_argument("--color", action="store_true", dest="color", required=False, help="enable coloring the output")
     global args
     args, system_argv = parser.parse_known_args()
 
-    ssh_server, max_len = get_ssh_server()
+    ssh_server = get_ssh_server()
     all_host = [i[0].lower() for i in ssh_server]
     if len(system_argv) < 2:
         if len(system_argv) == 1 and system_argv[0].lower() in all_host:
             start_ssh(ssh_server[all_host.index(system_argv[0].lower())][0],
                       ssh_server[all_host.index(system_argv[0].lower())][1],
-                      port=ssh_server[all_host.index(system_argv[0].lower())][3],
-                      ping=ssh_server[all_host.index(system_argv[0].lower())][4])
+                      port=ssh_server[all_host.index(system_argv[0].lower())][3])
         elif len(system_argv) == 1:
             global search
             search = system_argv[0]
     else:
         execlp('ssh', 'ssh', *system_argv)
 
-    rows, _ = popen('stty size', 'r').read().split()
-    start_prompter(ssh_server, max_len + 4, system_argv)
+    # rows, _ = popen('stty size', 'r').read().split()
+    start_prompter(ssh_server, system_argv)
 
 
 if __name__ == '__main__':
